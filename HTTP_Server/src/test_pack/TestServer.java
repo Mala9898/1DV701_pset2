06e711bc01bbed3884735a06d57122d0f0e1fc9b
VintageCake <lovesamuelsson@outlook.com>
2020-02-15 14:32:37 +0100

    Extended examples, ok receive implementation

--- HTTP_Server/src/test_pack/TestServer.java
+++ HTTP_Server/src/test_pack/TestServer.java
@@ -1,10 +1,30 @@
+/**
+ * @author: Love Samuelsson     (ls223qx@student.lnu.se)
+ * @author: Stanislaw J. Malec  (sm223ak@student.lnu.se)
+ * 2020-02-15
+ */
+
 package test_pack;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 
 public class TestServer {
+	private static File rootDir = null;
+
 	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("usage: specify root directory");
+			System.err.println("Continuing using default directory...");
+			rootDir = new File("");
+		}
+		else {
+			rootDir = getValidatedDir(args[0]);
+		}
+		System.out.println("Using file path: " + rootDir.getAbsolutePath());
+
+
 		ServerSocket server = null;
 		try {
 			server = new ServerSocket(80);
@@ -23,7 +43,18 @@ public class TestServer {
 		catch (IOException e) {
 			System.err.println("Thread creation failed: " + e.getMessage());
 		}
+	}
 
-
+	private static File getValidatedDir(String arg) {
+		File dir = new File(arg);
+		if (!dir.exists()) {
+			System.err.println("Invalid path");
+			System.exit(1);
+		}
+		else if (!dir.isDirectory()) {
+			System.err.println("Path is not a directory!");
+			System.exit(1);
+		}
+		return dir;
 	}
 }
--- HTTP_Server/src/test_pack/TestServerThread.java
+++ HTTP_Server/src/test_pack/TestServerThread.java
@@ -1,9 +1,16 @@
+/**
+ * @author: Love Samuelsson     (ls223qx@student.lnu.se)
+ * @author: Stanislaw J. Malec  (sm223ak@student.lnu.se)
+ * 2020-02-15
+ */
+
 package test_pack;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class TestServerThread implements Runnable {
 	private Socket socket;
@@ -14,35 +21,60 @@ public class TestServerThread implements Runnable {
 
 	@Override
 	public void run() {
-		InputStream in = null;
-		OutputStream out = null;
+		InputStream input = null;
+		OutputStream output = null;
 		try {
-			in = socket.getInputStream();
-			out = socket.getOutputStream();
+			input = socket.getInputStream();
+			output = socket.getOutputStream();
 		}
 		catch (IOException e) {
 			System.err.println("Write or read pipes broke on creation: " + e.getMessage());
 			System.exit(1);
 		}
+		byte[] bytes = new byte[2048];
+		StringBuilder lineFromClient = new StringBuilder();
+		int read;
+		boolean finishedReading = false;
+		String completeMessage = "";
 		try {
-			byte read;
+			MainLoop:
 			while (true) {
-				if ((read = (byte) in.read()) != -1) {
-					System.out.print((char) read);
+				String[] request = getRequest(input);
+				if (request.length == 1 && request[0].equals("")) {
+					System.out.println("Finished getting header");
+					finishedReading = true;
 				}
-				else {
-					System.out.println();
-					System.err.println("Connection from " + socket.getInetAddress().getHostAddress() + " was closed by host");
-					break;
-				}
-
 			}
 		}
 		catch (IOException e) {
 			System.err.println("Connection failed, reason: " + e.getMessage());
 			System.err.println("Closing connection: " + socket.getInetAddress());
-			System.exit(1);
 		}
+	}
 
+	private String[] getRequest(InputStream in) throws IOException {
+		int read;
+		StringBuilder lineFromClient = new StringBuilder();
+		String completeMessage;
+		boolean first = true;
+		while (true) {
+			if ((read = in.read()) != -1) {
+				System.out.print((char) read);
+				if (read == '\r' || read == '\n') {
+					if (first) {
+						first = false;
+						continue;
+					}
+					completeMessage = lineFromClient.toString();
+					return completeMessage.split("\\s+");
+				}
+				else {
+					lineFromClient.append((char) read);
+				}
+			}
+			else {
+				throw new IOException("Host closed connection");
+			}
+		}
 	}
 }
