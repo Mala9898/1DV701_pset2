1f2e8de036ecf2ac32940a4906a88f472717d7f1
Stan <6561374+Mala9898@users.noreply.github.com>
2020-02-15 10:48:55 +0100

    initial structure

---
+++ HTTP_Server/src/HTTPServer/ResponseBuilder.java
@@ -0,0 +1,9 @@
+package HTTPServer;
+
+/**
+ * @author: Stanislaw J. Malec  (sm223ak@student.lnu.se)
+ * @author: Love Samuelsson     (ls223qx@student.lnu.se)
+ * 2020-02-15
+ */
+public class ResponseBuilder {
+}
--- HTTP_Server/src/test_pack/TestClass.java
+++
@@ -1,7 +0,0 @@
-package test_pack;
-
-public class TestClass {
-	public static void main(String[] args) {
-		System.out.println("Hi");
-	}
-}
